e5b1f365092081f165b1809a7db71df8b5112016
Petar Filjak <pfiljak21@student.foi.hr>
2023-05-05 12:49:20 +0200

    Naravljenje su klase podataka

---
+++ EvaulationManager/EvaulationManager/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EvaulationManager {
+    abstract class Person {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public override string ToString() {
+            return Name + " " + LastNamey   
+        }
+    }
+}
---
+++ EvaulationManager/EvaulationManager/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaulationManager {
+    internal class Student : Person {
+        public int Grade { get; set; }
+    }
+}
